0c7f59dbf5bf92b1a22c51c2f00406ae16685193
fuyuakii <248933303+fuyuakii@users.noreply.github.com>
2025-12-14 08:53:20 -0300

    Update ex1.sql

--- DataBaseExercices/06_Introducao_Procedimentos/ex1.sql
+++ DataBaseExercices/06_Introducao_Procedimentos/ex1.sql
@@ -1,12 +1,9 @@
--- Criar ou substituir o banco de dados atividade1
 CREATE DATABASE IF NOT EXISTS atividade1;
 USE atividade1;
 
--- Definir o conjunto de caracteres e delimitador
 CHARSET utf8;
 DELIMITER //
 
--- Criar ou substituir a procedure Atividade1
 CREATE OR REPLACE PROCEDURE Atividade1(
     IN base DECIMAL(3,1),
     IN altura DECIMAL(3,1),
@@ -20,8 +17,6 @@ BEGIN
 END//
 DELIMITER ;
 
--- Executar o script atv.sql
 SOURCE atv.sql;
 
--- Chamar a procedure Atividade1 com parâmetros específicos
 CALL Atividade1(5.0, 3.0, @area, @perimetro);

a36974adf0d0d82962634c99b945935e8de00a15
fuyuakii <248933303+fuyuakii@users.noreply.github.com>
2025-12-14 08:55:12 -0300

    Update ex2.sql

--- DataBaseExercices/07_Funcoes_Matematicas/ex2.sql
+++ DataBaseExercices/07_Funcoes_Matematicas/ex2.sql
@@ -1,12 +1,9 @@
--- Criar ou substituir o banco de dados atividade2
 CREATE DATABASE IF NOT EXISTS atividade2;
 USE atividade2;
 
--- Definir o conjunto de caracteres e delimitador
 CHARSET utf8;
 DELIMITER //
 
--- Criar ou substituir a procedure atividade
 CREATE OR REPLACE PROCEDURE atividade(
     IN c1 DECIMAL(3,1),
     IN c2 DECIMAL(3,1),
@@ -24,5 +21,4 @@ DELIMITER ;
 -- Executar o script atv.sql
 SOURCE atv.sql;
 
--- Chamar a procedure atividade com valores específicos para c1 e c2 e exibir os resultados
 CALL atividade(3.0, 4.0, @c, @hipotenusa);

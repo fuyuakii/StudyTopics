418a7505aa6e207f7b0d853361d9a9db3d1f3ded
fuyuakii <248933303+fuyuakii@users.noreply.github.com>
2025-12-14 08:52:09 -0300

    Update ex3.sql

--- DataBaseExercices/03_Usuarios/ex3.sql
+++ DataBaseExercices/03_Usuarios/ex3.sql
@@ -1,10 +1,7 @@
--- 1. Criar o banco de dados petshop
 CREATE DATABASE IF NOT EXISTS petshop;
 
--- 2. Usar o banco de dados petshop
 USE petshop;
 
--- 3. Criar as tabelas
 CREATE TABLE Animal (
     Codigo INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(100) NOT NULL UNIQUE
@@ -27,15 +24,12 @@ CREATE TABLE Consulta (
     FOREIGN KEY (CodigoVeterinario) REFERENCES Veterinario(Codigo)
 );
 
--- 4. Criar os usuários
 CREATE USER 'ana'@'localhost' IDENTIFIED BY '12345678';
 CREATE USER 'bruno'@'localhost' IDENTIFIED BY '12345678';
 CREATE USER 'paulo'@'localhost' IDENTIFIED BY '12345678';
 
--- 5. Conceder permissões aos usuários
 GRANT ALL PRIVILEGES ON petshop.* TO 'ana'@'localhost';
 GRANT ALL PRIVILEGES ON petshop.* TO 'bruno'@'localhost';
 GRANT ALL PRIVILEGES ON petshop.* TO 'paulo'@'localhost';
 
--- 6. Liste os usuários criados
 SELECT user, host FROM mysql.user WHERE user IN ('ana', 'bruno', 'paulo') AND host='localhost';

3a5606ce18fdd478a0b883d71caceae52c9e3224
fuyuakii <248933303+fuyuakii@users.noreply.github.com>
2025-12-14 08:55:36 -0300

    Update ex4.sql

--- DataBaseExercices/07_Funcoes_Matematicas/ex4.sql
+++ DataBaseExercices/07_Funcoes_Matematicas/ex4.sql
@@ -1,12 +1,9 @@
--- Criar ou substituir o banco de dados atividade4
 CREATE DATABASE IF NOT EXISTS atividade4;
 USE atividade4;
 
--- Definir o conjunto de caracteres e delimitador
 CHARSET utf8;
 DELIMITER //
 
--- Criar ou substituir a procedure atividade
 CREATE OR REPLACE PROCEDURE atividade(
     IN n INT
 )
@@ -18,5 +15,4 @@ DELIMITER ;
 -- Executar o script atv.sql
 SOURCE atv.sql;
 
--- Chamar a procedure atividade com o valor específico para n e exibir o número aleatório gerado
 CALL atividade(2);

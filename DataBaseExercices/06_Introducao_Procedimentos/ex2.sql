c315dfe9962e9cd16df719b3aab668977bf4d764
fuyuakii <248933303+fuyuakii@users.noreply.github.com>
2025-12-14 08:53:43 -0300

    Update ex2.sql

--- DataBaseExercices/06_Introducao_Procedimentos/ex2.sql
+++ DataBaseExercices/06_Introducao_Procedimentos/ex2.sql
@@ -1,12 +1,9 @@
--- Criar ou substituir o banco de dados atividade2
 CREATE DATABASE IF NOT EXISTS atividade2;
 USE atividade2;
 
--- Definir o conjunto de caracteres e delimitador
 CHARSET utf8;
 DELIMITER //
 
--- Criar ou substituir a procedure atividade2
 CREATE OR REPLACE PROCEDURE atividade2(
     IN fahrenheit DECIMAL(3,1),
     OUT celsius DECIMAL(3,1)
@@ -20,5 +17,4 @@ DELIMITER ;
 -- Executar o script atv.sql
 SOURCE atv.sql;
 
--- Chamar a procedure atividade2 com o valor específico para fahrenheit e exibir os resultados
 CALL atividade2(5.0, @celsius);
